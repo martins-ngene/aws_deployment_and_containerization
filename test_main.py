ec23449d94338c2d4d4c9b6f196cb6b29cb3f343
Martins Ngene <martinsngene9@gmail.com>
2022-09-02 13:24:21 +0100

    [update]

--- test_main.py
+++ test_main.py
@@ -1,40 +1,37 @@
-'''
+"""
 Tests for jwt flask app.
-'''
+"""
 import os
 import json
 import pytest
 
 import main
 
-SECRET = 'TestSecret'
-TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+SECRET = "TestSecret"
+TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk"
+EMAIL = "wolf@thedoor.com"
+PASSWORD = "huff-puff"
+
 
 @pytest.fixture
 def client():
-    os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = True
+    os.environ["JWT_SECRET"] = SECRET
+    main.APP.config["TESTING"] = True
     client = main.APP.test_client()
 
     yield client
 
 
-
 def test_health(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json == 'Healthy'
+    assert response.json == "Jesus is real, The Holy Spirit speaks, God never changes."
 
 
 def test_auth(client):
-    body = {'email': EMAIL,
-            'password': PASSWORD}
-    response = client.post('/auth', 
-                           data=json.dumps(body),
-                           content_type='application/json')
+    body = {"email": EMAIL, "password": PASSWORD}
+    response = client.post("/auth", data=json.dumps(body), content_type="application/json")
 
     assert response.status_code == 200
-    token = response.json['token']
+    token = response.json["token"]
     assert token is not None
